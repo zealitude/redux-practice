04af7415423b43d41479204751e9b762d08e2e89
zealitude <zealitude@gmail.com>
2017-02-26 19:12:48 +0800

    simple counter using ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,19 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/counter';
-import { ToDoListModule } from './to-do-list/to-do-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // CounterComponent,
+    CounterComponent,
 ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ToDoListModule,
     // { key : value }
     // { storeName : Reducer }
-    // StoreModule.provideStore({counter: counterReducer})
+    StoreModule.provideStore({counter: counterReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
